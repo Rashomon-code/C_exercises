d373acaffa9793f9240a6c61817254901a9d738c
Rashomon-code <cn475@icloud.com>
2025-10-11 21:55:06 +0900

    Fix logic for guessing game random number

--- number_guessing_game/src/main.c
+++ number_guessing_game/src/main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "random.h"
 
 int main(void){
+    srand(time(NULL));
     int num = create_num(1, 100);
     printf("%d\n", num);
     return 0;
--- number_guessing_game/src/random.c
+++ number_guessing_game/src/random.c
@@ -3,6 +3,5 @@
 #include <stdlib.h>
 
 int create_num(int min, int max){
-    srand(time(NULL));
     return rand() / (RAND_MAX / (max - min +1)) + min;
 }
